39ce172ef999e93525d5c78aaa64047cbad61b01
Sami McGuire <samimcguire@gmail.com>
2023-09-24 14:48:42 -0700

    nav bar hover add

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,18 @@
 import { useContext } from "react";
 import { UserContext } from "../context";
+import Logo from "./badbanklogo.png"
 import Card from 'react-bootstrap/Card';
 
 const Home = () => {
     return (
-        <Card style={{  }}>
+        <Card style={{ width: '18rem', top: 'center'}}>
         <Card.Header style={{ fontFamily:"cursive", fontWeight:"bold" }}>BadBank Landing Page</Card.Header>
         <Card.Body>
           <Card.Title>Welcome to the Bank</Card.Title>
           <Card.Text>
             You can use this bank
           </Card.Text>
-          <Card.Img src=".\badbanklogo.png"  className="img-fluid" alt="responsive bank logo"/>
+          <Card.Img src={Logo} className="img-fluid" alt="responsive bank logo"/>
         </Card.Body>
       </Card>
     );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,12 +3,12 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
    return (
     <ul className="nav justify-content-center nav-tabs">
-        <Link to='/' className="nav-link" ><li className="nav-item">Home</li></Link>
-        <Link to="/createaccount" className="nav-link"><li className="nav-item">Create Account</li></Link>
-        <Link to="/login" className="nav-link"><li className="nav-item">Login</li></Link>
-        <Link to="/deposit" className="nav-link"><li className="nav-item">Deposit</li></Link>
-        <Link to="/withdraw" className="nav-link"><li className="nav-item">Withdraw</li></Link>
-        <Link to="/alldata" className="nav-link"><li className="nav-item">All Data</li></Link>
+        <Link to='/' className="nav-link" ><li className="nav-item" data-toggle="tooltip" data-placement="top" title="Navigate to Bank home page">Home</li></Link>
+        <Link to="/createaccount" className="nav-link"><li className="nav-item" data-toggle="tooltip" data-placement="top" title="Create your Bank Account">Create Account</li></Link>
+        <Link to="/login" className="nav-link"><li className="nav-item" data-toggle="tooltip" data-placement="top" title="Login to your account">Login</li></Link>
+        <Link to="/deposit" className="nav-link"><li className="nav-item" data-toggle="tooltip" data-placement="top" title="Navigate to deposit funds">Deposit</li></Link>
+        <Link to="/withdraw" className="nav-link"><li className="nav-item" data-toggle="tooltip" data-placement="top" title="Navigate to withdraw funds">Withdraw</li></Link>
+        <Link to="/alldata" className="nav-link"><li className="nav-item" data-toggle="tooltip" data-placement="top" title="Navigate to see all data">All Data</li></Link>
     </ul>
    ); 
 }
