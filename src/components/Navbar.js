58a05042bef04acd2f9bcb288f4c50bbbb72b48a
Sami McGuire <samimcguire@gmail.com>
2023-09-24 14:02:18 -0700

    update nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
    return (
     <ul className="nav justify-content-center nav-tabs">
-        <Link to='/' className="nav-link"><li className="nav-item">BadBank</li></Link>
+        <Link to='/' className="nav-link" ><li className="nav-item">Home</li></Link>
         <Link to="/createaccount" className="nav-link"><li className="nav-item">Create Account</li></Link>
         <Link to="/login" className="nav-link"><li className="nav-item">Login</li></Link>
         <Link to="/deposit" className="nav-link"><li className="nav-item">Deposit</li></Link>
