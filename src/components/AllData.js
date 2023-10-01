83420ba086add5e83a885b09642b8c54cd88d601
Sami McGuire <samimcguire@gmail.com>
2023-10-01 16:10:13 -0700

    add JSON data

--- src/components/AllData.js
+++ src/components/AllData.js
@@ -1,12 +1,35 @@
 import { useContext } from "react";
 import { UserContext } from "../context";
+import Card from 'react-bootstrap/Card';
+import Table from 'react-bootstrap/Table';
 
 const AllData = () => {
     const ctx = useContext(UserContext);
     return (
-        <h1>All Data<br/>
-            {JSON.stringify(ctx)}
-        </h1>
+        <Card style={{ width: '50rem', top: 'center', left: '15rem' }}>
+            <Card.Header style={{ fontWeight:"bold" }}>All Data</Card.Header>
+            <Card.Body>
+                {JSON.stringify(ctx)}
+                <Table>
+                    <thead>
+                        <tr>
+                        <th data-field="ctx.name">Name</th>
+                        <th data-field="ctx.email">Email</th>
+                        <th data-field="ctx.password">Password</th>
+                        <th data-field="ctx.balance">Balance</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>{JSON.stringify(ctx.name)}</td>
+                            <td>{JSON.stringify(ctx.email)}</td>
+                            <td>{JSON.stringify(ctx.password)}</td>
+                            <td>{JSON.stringify(ctx.balance)}</td>
+                        </tr>
+                    </tbody>
+                    </Table>
+            </Card.Body>
+        </Card>
     );
 }
 
