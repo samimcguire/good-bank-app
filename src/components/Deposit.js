772baf0291fe3f145f343679ff96c6fc826e23d9
Sami McGuire <samimcguire@gmail.com>
2023-10-01 14:27:03 -0700

    Depost and Withdraw compnent creation

--- src/components/Deposit.js
+++ src/components/Deposit.js
@@ -1,12 +1,61 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { UserContext } from "../context";
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
 const Deposit = () => {
+    // Balance is always initially set to 100
+    const [deposit, setDeposit] = useState('');
+    const [balance, setBalance] = useState(100);
+    const [validTransaction, setValidTransaction] = useState(false);
     const ctx = useContext(UserContext);
+
+    let status = `BALANCE:    $${balance}`;
+
+    const handleChange = (event) => {
+        console.log(Number(event.target.value));
+        if (Number(event.target.value) <= 0) {
+        return setValidTransaction(false);
+        }
+        if (Number(event.target.value) > balance) {
+        setValidTransaction(false);
+        } else {
+        setValidTransaction(true);
+        }
+        setDeposit(Number(event.target.value));
+    };
+
+    const handleSubmit = (event) => {
+        let newBalance = balance + deposit;
+        setBalance(newBalance);
+        setValidTransaction(false);
+        event.preventDefault();
+        ctx.users.push({balance});
+    };
+
     return (
-        <h1>Deposit<br/>
-            {JSON.stringify(ctx)}
-        </h1>
+        <Card style={{ width: '50rem', top: 'center', left: '15rem' }}>
+            <Card.Header style={{ fontWeight:"bold" }}>Deposit</Card.Header>
+            <Card.Body>
+                <Form>
+                    <Form.Group className="mb-3">
+                        <Form.Text id="total">
+                        {status}
+                        </Form.Text>
+                    </Form.Group>
+
+                    <Form.Group className="mb-3">
+                        <Form.Label>Deposit Amount</Form.Label>
+                        <Form.Control type="number" id="deposit" placeholder="Enter deposit amount" value={deposit} onChange={handleChange} />
+                    </Form.Group>
+
+                    <Button variant="primary" type="submit"  onClick={handleSubmit}>
+                        Deposit
+                    </Button>
+                </Form>
+            </Card.Body>
+        </Card>
     );
 };
 
--- src/components/Withdraw.js
+++ src/components/Withdraw.js
@@ -1,13 +1,62 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { UserContext } from "../context";
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
 const Withdraw = () => {
+    // Balance is always initially set to 100
+    const [withdraw, setWithdraw] = useState('');
+    const [balance, setBalance] = useState(100);
+    const [validTransaction, setValidTransaction] = useState(false);
     const ctx = useContext(UserContext);
+
+    let status = `BALANCE:    $${balance}`;
+
+    const handleChange = (event) => {
+        console.log(Number(event.target.value));
+        if (Number(event.target.value) <= 0) {
+        return setValidTransaction(false);
+        }
+        if (Number(event.target.value) > balance) {
+        setValidTransaction(false);
+        } else {
+        setValidTransaction(true);
+        }
+        setWithdraw(Number(event.target.value));
+    };
+
+    const handleSubmit = (event) => {
+        let newBalance = balance - withdraw;
+        setBalance(newBalance);
+        setValidTransaction(false);
+        event.preventDefault();
+        ctx.users.push({balance});
+    };
+
     return (
-        <h1>Withdraw<br/>
-            {JSON.stringify(ctx)}
-        </h1>
+        <Card style={{ width: '50rem', top: 'center', left: '15rem' }}>
+            <Card.Header style={{ fontWeight:"bold" }}>Withdraw</Card.Header>
+            <Card.Body>
+                <Form>
+                    <Form.Group className="mb-3">
+                        <Form.Text id="total">
+                        {status}
+                        </Form.Text>
+                    </Form.Group>
+
+                    <Form.Group className="mb-3">
+                        <Form.Label>Withdraw Amount</Form.Label>
+                        <Form.Control type="number" id="withdraw" placeholder="Enter amount to withdraw" value={withdraw} onChange={handleChange} />
+                    </Form.Group>
+
+                    <Button variant="primary" type="submit"  onClick={handleSubmit}>
+                        Withdraw
+                    </Button>
+                </Form>
+            </Card.Body>
+        </Card>
     );
-}
+};
 
 export default Withdraw;
